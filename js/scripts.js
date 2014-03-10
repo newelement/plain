83c81a7e9eaf2f070f19900c963487d51cdc5216
Don Jones <djones@newelementdesigns.com>
2014-03-09 23:32:18 -0400

    Fixed sidr nav bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
+var globalScrollCheck = false;
+var viewportWidth = $(window).width();
+
+function removeSticky(){
+
+    $('body').removeClass("sticky");
+
+}
+
 $(document).ready(function() {
 
     $('.offcanvas-toggle').sidr({
         name: 'mobile-mav',
-        source: '.main-nav'
+        source: '.main-nav',
+        body : '#site-body'
     });
 
-    
+    $(window).on("scroll", function() {
+
+        if(viewportWidth > 767){
+            var fromTop = $(this).scrollTop();
+            $('body').toggleClass("sticky", (fromTop > 60));
+            globalScrollCheck = false;
+        }
+
+    });
+
+
+    $(window).resize(function() {
+
+        viewportWidth = $(window).width();
+
+        if(viewportWidth < 768 && globalScrollCheck === false){
+            removeSticky();
+            globalScrollCheck = true;
+        }
+
+    });
+
 });
