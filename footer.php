17545e520cb488403cffe54a741cd86d40aabfe4
Don Jones <djones@newelementdesigns.com>
2014-03-11 08:23:01 -0400

    Added sharrre plugin and blog page

---
+++ footer.php
@@ -0,0 +1,41 @@
+            <footer class="footer">
+                <div class="container">
+                    <div class="span4">
+                        <nav class="footer-nav">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Pages</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div><!-- END .span4 -->
+                    <div class="span4 socials-footer">
+                        <a class="social-button" href="#"><i class="fa fa-twitter"></i></a> 
+                        <a class="social-button" href="#"><i class="fa fa-instagram"></i></a> 
+                        <a class="social-button" href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="social-button" href="#"><i class="fa fa-google-plus"></i></a>
+                        <a class="social-button" href="#"><i class="fa fa-pinterest"></i></a>
+                    </div>
+                    <div class="span4 copyright">
+                        <p>&copy; Copyright &infin; Plain. Website</p>
+                    </div>
+                </div>
+            </footer><!-- END .footer -->
+        
+        
+    
+        </div><!-- END #site-body -->
+    
+        <script src="js/scripts.min.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-XXXXX-YY');
+          ga('send', 'pageview');
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php $home = true ?>
+<?php include('header.php') ?>
+            
+            
+            <section class="container section">
+                <div class="row">
+                    
+                    <div class="span7">
+                        
+                        <h1>Plain Website</h1>
+                
+                        <p>
+                        lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        
+                        <h1>Heading 1</h1>
+                        <h2>Heading 2</h2>
+                        <h3>Heading 3</h3>
+                        <h4>Heading 4</h4>
+                        <h5>Heading 5</h5>
+                        <h6>Heading 6</h6>
+                        
+                        <img src="http://placehold.it/250x150" alt="placeholder" width="250" height="150" class="align-right max-width">
+                        
+                        <p>
+                        Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. 
+                        </p>
+                        
+                        
+                        <ul>
+                            <li>Aenean lacinia bibendum nulla</li>
+                            <li>Donec ullamcorper nulla non</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                        </ul>
+                        
+                    
+                    </div><!-- END .span7 -->
+                    
+                    
+                    <aside class="span4 pull-right sidebar">
+                        <h3>My Sidebar</h3>
+                        <p>
+                        lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <img src="http://placehold.it/300x200" alt="placeholder" width="300" height="200" class="max-width">
+                    </aside><!-- END .sidebar -->
+                    
+                </div> <!-- END .row -->
+            </section><!-- // END .container.section -->
+    
+            
+            
+<?php include('footer.php'); ?>
